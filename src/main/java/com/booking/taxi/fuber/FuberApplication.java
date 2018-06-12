87f50afbd39207ec527c8213d5ffc0a0814928a3
VIGNESH <VIGNESH@DESKTOP-SVNCDE6>
2018-06-13 00:46:42 +0530

    refs #master springboot app defined

---
+++ src/main/java/com/booking/taxi/fuber/FuberApplication.java
@@ -0,0 +1,13 @@
+package com.booking.taxi.fuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuberApplication {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(FuberApplication.class, args);
+    }
+
+}
